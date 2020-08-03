07d49364ca4f18455f16003900e4ba7e6eaa32c0
dat25134 <tlvdat.bkdn@gmail.com>
2020-08-03 14:53:41 +0700

    khởi tạo dự án laravel + get package laravel ui

---
+++ database/migrations/2020_08_03_075049_create_khuvucs_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateKhuvucsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('khuvucs', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('khuvucs');
+    }
+}
